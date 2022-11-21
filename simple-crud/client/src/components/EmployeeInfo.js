17bbf74011aed1b0b52d2c2b2f1dfa7ae6e8fa0c
jbenite2 <72776470+jbenite2@users.noreply.github.com>
2022-11-21 02:43:01 -0500

    Alts added the js file

--- simple-crud/client/src/components/EmployeeInfo.js
+++ simple-crud/client/src/components/EmployeeInfo.js
@@ -10,19 +10,31 @@ function EmployeeInfo() {
 
       <div className="row">
         <div className="clm">
-          <img src={require('../assets/rocco.jpg')} height="300"></img>
+          <img
+            src={require('../assets/rocco.jpg')}
+            alt="Rocco"
+            height="300"
+          ></img>
           <h3> Justin "Rocco" Pajak</h3>
           <h3>Class: Senior</h3>
           <h3>Section: 3B</h3>
         </div>
         <div className="clm">
-          <img src={require('../assets/mino.jpg')} height="300"></img>
+          <img
+            src={require('../assets/mino.jpg')}
+            alt="Mino"
+            height="300"
+          ></img>
           <h3> Gilberto "Mino" Cerda </h3>
           <h3>Class: Junior</h3>
           <h3>Section: 3B</h3>
         </div>
         <div className="clm">
-          <img src={require('../assets/hacker.jpg')} height="300"></img>
+          <img
+            src={require('../assets/hacker.jpg')}
+            alt="Hacker"
+            height="300"
+          ></img>
           <h3> Matthew "Hacker" Schoessling</h3>
           <h3>Class: Senior</h3>
           <h3>Section: 4B</h3>
@@ -30,19 +42,31 @@ function EmployeeInfo() {
       </div>
       <div className="row">
         <div className="clm">
-          <img src={require('../assets/turbo.jpg')} height="300"></img>
+          <img
+            src={require('../assets/turbo.jpg')}
+            alt="Turbo"
+            height="300"
+          ></img>
           <h3> Brendan "Turbo" Noland </h3>
           <h3>Class: Sophomore</h3>
           <h3>Section: 4A</h3>
         </div>
         <div className="clm">
-          <img src={require('../assets/biggie.png')} height="300"></img>
+          <img
+            src={require('../assets/biggie.png')}
+            alt="Biggie Cheese"
+            height="300"
+          ></img>
           <h3> Colby "Biggie Cheese" Rossman </h3>
           <h3>Class: Junior</h3>
           <h3>Section: 1A</h3>
         </div>
         <div className="clm">
-          <img src={require('../assets/hacker.jpg')} height="300"></img>
+          <img
+            src={require('../assets/hacker.jpg')}
+            alt="Nilla"
+            height="300"
+          ></img>
           <h3> Kevin "Nilla" Lance</h3>
           <h3>Class: Junior</h3>
           <h3>Section: 3B</h3>
