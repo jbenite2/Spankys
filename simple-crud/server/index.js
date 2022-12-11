fb1015edef40061f3db29eedf5a4ef4b48f49870
jbenite2 <72776470+jbenite2@users.noreply.github.com>
2022-12-11 16:33:50 -0500

    port changed to 3001

--- simple-crud/server/index.js
+++ simple-crud/server/index.js
@@ -1,66 +1,64 @@
 const express = require('express') //Instance of the express library
 const app = express() //Creating an app from the instance
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 const mysql = require('mysql2') //Setting up MySQL. Note that we need mysql2
 const cors = require('cors') //Cross-origin resource sharing
 const { stepperClasses } = require('@mui/material')
 require('dotenv').config() //.env file in this folder holds private information
-const basicAuth = require('express-basic-auth');
-const cookieParser = require('cookie-parser');
-var Venmo = require('venmo');
-var client_id = process.env.VENMO_CLIENT_ID;
-var client_secret = process.env.VENMO_SECRET;
-var venmo = new Venmo(client_id, client_secret);
+const basicAuth = require('express-basic-auth')
+const cookieParser = require('cookie-parser')
+var Venmo = require('venmo')
+var client_id = process.env.VENMO_CLIENT_ID
+var client_secret = process.env.VENMO_SECRET
+var venmo = new Venmo(client_id, client_secret)
 const open = require('open')
 
-app.use(cookieParser('82e4e438a0705fabf61f9854e3b575af'));
+app.use(cookieParser('82e4e438a0705fabf61f9854e3b575af'))
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/login', (req, res) => {
-  if (req.body.username == 'spankys' && req.body.password == 'spankys22'){
-  res.send({
-    token: 'spankys'
-  });
-}
-else {
-  console.log("Error - incorrect credentials")
-  res.send('');
-}
-});
+  if (req.body.username == 'spankys' && req.body.password == 'spankys22') {
+    res.send({
+      token: 'spankys',
+    })
+  } else {
+    console.log('Error - incorrect credentials')
+    res.send('')
+  }
+})
 
 var object = {
-  user:'mattschoess'
+  user: 'mattschoess',
 }
 
 app.post('/pay', (req, res) => {
-  venmo.pay(object, function(error, link) {
+  venmo.pay(object, function (error, link) {
     if (error) {
-      console.log(error);
+      console.log(error)
     } else {
-      open(link, function(err) {
-        if (err) throw err;
-      });
+      open(link, function (err) {
+        if (err) throw err
+      })
       console.log(link)
     }
   })
 })
 
-const current = new Date();
+const current = new Date()
 
 let transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: 'gmail',
   auth: {
-    type: "OAuth2",
+    type: 'OAuth2',
     user: process.env.EMAIL,
     pass: process.env.WORD,
     clientId: process.env.OAUTH_CLIENTID,
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
   },
- });
-
+})
 
 const db = mysql.createConnection(process.env.DATABASE_URL)
 console.log('Connected to PlanetScale!')
@@ -85,7 +83,6 @@ app.post('/create', (req, res) => {
   )
 })
 
-
 app.get('/orders', (req, res) => {
   db.query('SELECT * FROM orders22', (err, result) => {
     if (err) {
@@ -95,7 +92,6 @@ app.get('/orders', (req, res) => {
     }
   })
 })
-
 
 app.get('/inventory', (req, res) => {
   db.query('SELECT * FROM inventory', (err, result) => {
@@ -107,10 +103,8 @@ app.get('/inventory', (req, res) => {
   })
 })
 
-
 app.get('/openorders', (req, res) => {
-  db.query('SELECT * FROM orders22 WHERE completed = 0', 
-  (err, result) => {
+  db.query('SELECT * FROM orders22 WHERE completed = 0', (err, result) => {
     if (err) {
       console.log(err)
     } else {
@@ -120,8 +114,7 @@ app.get('/openorders', (req, res) => {
 })
 
 app.get('/countopenorders', (req, res) => {
-  db.query('SELECT * FROM orders22 WHERE completed = 0', 
-  (err, result) => {
+  db.query('SELECT * FROM orders22 WHERE completed = 0', (err, result) => {
     if (err) {
       console.log(err)
     } else {
@@ -130,7 +123,6 @@ app.get('/countopenorders', (req, res) => {
     }
   })
 })
-
 
 app.put('/update', (req, res) => {
   const id = req.body.id
@@ -148,7 +140,6 @@ app.put('/update', (req, res) => {
   )
 })
 
-
 app.put('/updateInventory', (req, res) => {
   const Item = req.body.Item
   const QuantityLeft = req.body.QuantityLeft
@@ -163,7 +154,6 @@ app.put('/updateInventory', (req, res) => {
     },
   )
 })
-
 
 app.put('/updateq', (req, res) => {
   const id = req.body.id
@@ -208,89 +198,95 @@ app.delete('/delete/:id', (req, res) => {
   })
 })
 
-
-
-
-
-
 transporter.verify((err, success) => {
   err
     ? console.log(err)
-    : console.log(`=== Server is ready to take messages: ${success} ===`);
- });
-
-
- app.post("/send", function (req, res) {
-
-  const message = 'Hello '+(req.body.nickname)+',\n Your order is ready for pickup!\n\n Order details:\n\n Order item: '+(req.body.item)+'\n Quantity: '+(req.body.qty)+'\n Email: '+(req.body.phone)+'\n There are '+(req.body.numOpen)+' orders ahead of you.\n Thank you for your business!'
-
-
-  let mailOptions = {
-    from: process.env.EMAIL,
-    to: req.body.phone,
-    subject: "Order ready for pickup",
-    text: message,
-  };
- 
-  transporter.sendMail(mailOptions, function (err, data) {
-    if (err) {
-      console.log("Error " + err);
-    } else {
-      console.log("Email sent successfully");
-    }
-  });
- });
-
- app.post("/sendconf", function (req, res) {
-
-  const message = 'Hello '+(req.body.nickname)+',\n Here is your order confirmation:\n\n Order details:\n\n Order item: '+(req.body.item)+'\n Quantity: '+(req.body.qty)+'\n Email: '+(req.body.phone)+'\n There are '+(req.body.numOpen)+' orders ahead of you.'
-
-
-  let mailOptions = {
-    from: process.env.EMAIL,
-    to: req.body.phone,
-    subject: "Order confirmation",
-    text: message,
-  };
- 
-  transporter.sendMail(mailOptions, function (err, data) {
-    if (err) {
-      console.log("Error " + err);
-    } else {
-      console.log("Email sent successfully");
-    }
-  });
- });
-
- app.post("/sendcancel", function (req, res) {
-
-  const message = 'Hello '+(req.body.nickname)+',\n Your order has been canceled.\n\n Reason:\n\n'+(req.body.message)
-
-
-  let mailOptions = {
-    from: process.env.EMAIL,
-    to: req.body.phone,
-    subject: "Order cancelled",
-    text: message,
-  };
- 
-  transporter.sendMail(mailOptions, function (err, data) {
-    if (err) {
-      console.log("Error " + err);
-    } else {
-      console.log("Email sent successfully");
-    }
-  });
- });
-
-
-  
-
-
-
-app.listen(8080, () => {
-  console.log('Yay, your server is running in port 8080')
+    : console.log(`=== Server is ready to take messages: ${success} ===`)
 })
 
+app.post('/send', function (req, res) {
+  const message =
+    'Hello ' +
+    req.body.nickname +
+    ',\n Your order is ready for pickup!\n\n Order details:\n\n Order item: ' +
+    req.body.item +
+    '\n Quantity: ' +
+    req.body.qty +
+    '\n Email: ' +
+    req.body.phone +
+    '\n There are ' +
+    req.body.numOpen +
+    ' orders ahead of you.\n Thank you for your business!'
 
- 
+  let mailOptions = {
+    from: process.env.EMAIL,
+    to: req.body.phone,
+    subject: 'Order ready for pickup',
+    text: message,
+  }
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log('Error ' + err)
+    } else {
+      console.log('Email sent successfully')
+    }
+  })
+})
+
+app.post('/sendconf', function (req, res) {
+  const message =
+    'Hello ' +
+    req.body.nickname +
+    ',\n Here is your order confirmation:\n\n Order details:\n\n Order item: ' +
+    req.body.item +
+    '\n Quantity: ' +
+    req.body.qty +
+    '\n Email: ' +
+    req.body.phone +
+    '\n There are ' +
+    req.body.numOpen +
+    ' orders ahead of you.'
+
+  let mailOptions = {
+    from: process.env.EMAIL,
+    to: req.body.phone,
+    subject: 'Order confirmation',
+    text: message,
+  }
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log('Error ' + err)
+    } else {
+      console.log('Email sent successfully')
+    }
+  })
+})
+
+app.post('/sendcancel', function (req, res) {
+  const message =
+    'Hello ' +
+    req.body.nickname +
+    ',\n Your order has been canceled.\n\n Reason:\n\n' +
+    req.body.message
+
+  let mailOptions = {
+    from: process.env.EMAIL,
+    to: req.body.phone,
+    subject: 'Order cancelled',
+    text: message,
+  }
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log('Error ' + err)
+    } else {
+      console.log('Email sent successfully')
+    }
+  })
+})
+
+app.listen(3001, () => {
+  console.log('Yay, your server is running in port 3001')
+})
