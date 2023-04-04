6d7beb2905278c90fe10dbf1bac6d45302776695
Jose <jbenite2@nd.edu>
2023-04-04 11:31:30 -0700

    mobile made

--- simple-crud/client/src/components/Home.js
+++ simple-crud/client/src/components/Home.js
@@ -1,43 +1,79 @@
 import React from 'react';
-import logo from '../assets/Spankys.png'
+import logo from '../assets/Spankys.png';
 import Button from '@mui/material/Button';
-import './Home.css'
+import './Home.css';
 import Typography from '@mui/material/Typography';
-import {Routes, Route, useNavigate} from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
-  
-function Home(){
+function Home() {
+  const navigate = useNavigate();
 
-	const navigate = useNavigate();
+  const navigateToCustomer = () => {
+    navigate('/Customer');
+  };
 
-	const navigateToCustomer = () => {
-		 navigate('/Customer');
-	}
+  const navigateEmployee = () => {
+    navigate('/employee');
+  };
 
-	const navigateEmployee =() => {
-		 navigate('/employee');
-	}
-
-
-    return (
-        <div className="page">
-        <div className="home">
-            
-        
-            <div className="choices">
-            <h1 className="title">Welcome to Spanky's!</h1>
-            <h4 className="text">I am a ...</h4>
-                <Button className="button" onClick={navigateToCustomer} variant='outlined'
-  				sx={{ color: 'white', backgroundColor: 'orange', borderColor: 'white', minWidth: '30%', padding: '10px', margin: '20px' }}>Customer</Button>
-                <Button className="button" onClick={navigateEmployee} variant='outlined' 
-					 sx={{ color: 'white', backgroundColor: 'orange', borderColor: 'white', minWidth: '30%', padding: '10px', margin: '20px' }}>Employee</Button>
-            </div>
-            <div className="choices2">
-            <img src={logo} alt="spanky's logo" className="logo"></img>
-            </div>
+  return (
+    <div className="page">
+      <div className="home">
+        <div className="choices2">
+          <img src={logo} alt="spanky's logo" className="logo" />
         </div>
+        <div className="choices" style={{ backgroundColor: 'orange' }}>
+          <Typography
+            variant="h4"
+            component="h1"
+            align="center"
+            gutterBottom
+            style={{ color: 'white' }}
+          >
+            Welcome to Spanky's!
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            gutterBottom
+            style={{ color: 'white' }}
+          >
+            I am a ...
+          </Typography>
+          <Button
+            className="button"
+            onClick={navigateToCustomer}
+            variant="contained"
+            sx={{
+              color: 'white',
+              backgroundColor: 'orange',
+              borderColor: 'white',
+              minWidth: '100%',
+              padding: '10px',
+              margin: '20px',
+            }}
+          >
+            Customer
+          </Button>
+          <Button
+            className="button"
+            onClick={navigateEmployee}
+            variant="contained"
+            sx={{
+              color: 'white',
+              backgroundColor: 'orange',
+              borderColor: 'white',
+              minWidth: '100%',
+              padding: '10px',
+              margin: '20px',
+            }}
+          >
+            Employee
+          </Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-  
+
 export default Home;
